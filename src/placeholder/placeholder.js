5209cd7a42e365351eaa1da8467d7174a4d9854a
dolymood <dolymood@gmail.com>
2014-05-08 19:20:13 +0800

    c

--- src/placeholder/placeholder.js
+++ src/placeholder/placeholder.js
@@ -6,14 +6,31 @@ define(['angular'], function(angular) {
 	var supportPlaceHoder = 'placeholder' in document.createElement('input');
 
 	if (!supportPlaceHoder) {
+		var changeInputType = function(oldObject, oType) {
+			var newObject = document.createElement('input');
+			newObject.type = oType;
+			if(oldObject.className) newObject.className = oldObject.className;
+			oldObject.parentNode.insertBefore(newObject, oldObject);
+			return newObject;
+		}
 		md.directive('placeholder', ['$timeout', function($timeout) {
 		
 			return function(scope, elem, attrs) {
 				var txt = attrs.placeholder;
 				if (!txt) return;
+				var newInput;
+				newInput = angular.element(changeInputType(elem[0], 'text'));
+				newInput.val(txt);
+				newInput.on('focus', function() {
+					newInput.css('display', 'none');
+					elem.css('display', 'inline-block')[0].focus();
+				});
+				elem.css('display', 'none');
+
 				var modelT = attrs.ngModel;
+
 				elem.on('focus', function() {
-					if (modelT ? !scope.$eval(modelT) : (elem.val() === txt)) {
+					if (modelT ? !scope.$eval(modelT) : !elem.val()) {
 						elem.val('');
 					}
 					scope.$apply();
@@ -21,13 +38,20 @@ define(['angular'], function(angular) {
 
 				elem.on('blur', function() {
 					if (elem.val() === '') {
-						elem.val(txt);
+						newInput.css('display', 'inline-block');
+						elem.css('display', 'none');
 					}
 					scope.$apply();
 				});
 
 				$timeout(function() {
-					if (!elem.val()) elem.val(txt);
+					if (!elem.val()) {
+						newInput.css('display', 'inline-block');
+						elem.css('display', 'none');
+					} else {
+						newInput.css('display', 'none');
+						elem.css('display', 'inline-block');
+					}
 					scope.$apply();
 				});
 			}
